244ab848acc005323e218d456401d59c46a3f349
deface90 <deface06@yandex.ru>
2020-09-09 15:36:19 +0500

    export fields

--- formatter.go
+++ formatter.go
@@ -6,14 +6,14 @@ import (
 
 // formatter adds default fields to each log entry.
 type Formatter struct {
-	fields log.Fields
-	lf     log.Formatter
+	Fields log.Fields
+	LF     log.Formatter
 }
 
 // Format satisfies the logrus.Formatter interface.
 func (f *Formatter) Format(e *log.Entry) ([]byte, error) {
-	for k, v := range f.fields {
+	for k, v := range f.Fields {
 		e.Data[k] = v
 	}
-	return f.lf.Format(e)
+	return f.LF.Format(e)
 }
