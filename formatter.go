5d959ef81c59f1a048a6d5c54bd1110d261dad81
deface90 <deface06@yandex.ru>
2020-09-09 15:00:34 +0500

    add formatter

---
+++ formatter.go
@@ -0,0 +1,15 @@
+package logrus_sentry
+
+// formatter adds default fields to each log entry.
+type Formatter struct {
+	fields log.Fields
+	lf     log.Formatter
+}
+
+// Format satisfies the logrus.Formatter interface.
+func (f *Formatter) Format(e *log.Entry) ([]byte, error) {
+	for k, v := range f.fields {
+		e.Data[k] = v
+	}
+	return f.lf.Format(e)
+}
